35bcd0a93aa7cfd5ec65a42806624cf536c31d19
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-01-20 15:33:40 +0600

    commited

---
+++ lab2.py
@@ -0,0 +1,46 @@
+#PYTHON Booleans
+#exercise 1
+print(10 > 9)
+
+True
+
+#exercise 2
+print(10 == 9)
+
+False
+ 
+#exercise 3
+print(10 < 9)
+
+False
+
+#exercise 4
+print(bool("abc"))
+
+True
+
+#exercise 5
+print(bool(0))
+
+False
+
+#PYTHON Operators
+#exercise 1
+print(10*5)
+
+#exercise 2
+print(10/2)
+
+#exercise 3
+fruits = ["apple", "banana"]
+if "apple" in fruits:
+    print("Yes, apple is a fruit!")
+
+#exercise 4
+if 5 != 10:
+    print("5 and 10 is not equal")
+
+#exercise 5
+
+
+
