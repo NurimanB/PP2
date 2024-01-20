a16509088f7fae10ceb9cb8aa765e61793236ad9
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-01-20 17:44:23 +0600

    commited

--- lab2.py
+++ lab2.py
@@ -41,6 +41,42 @@ if 5 != 10:
     print("5 and 10 is not equal")
 
 #exercise 5
+if 5 == 10 or 4 == 4:
+    print("At least one of the statements is true")
 
+#PYTHON Lists
+#exercise 1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
 
+#exercise 2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+#exercise 3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#exercise 4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+
+#exercise 5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#exercise 6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#exercise 7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#exercise 8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+#PYTHON Tuples
+#exercise 1
 
