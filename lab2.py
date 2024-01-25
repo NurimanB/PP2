08850e7ea836f7d5c66fb941b6be063ea7ab34f1
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-01-25 12:43:51 +0600

    commited

--- lab2.py
+++ lab2.py
@@ -219,3 +219,50 @@ i = 1
 while i < 6:
     print(i)
     i += 1
+
+#exercise 2
+i = 1
+while i < 6:
+    if i == 3:
+        break
+    i += 1
+
+#exercise 3
+i=0
+while i<6:
+    i+=1
+    if i==3:
+        continue
+    print(i)
+
+#exercise 4
+i=1
+while i<6:
+    print(i)
+    i+=1
+else:
+    print("i is no longer less than 6")
+
+#PYTHON For loops
+#exercise 1
+fruits=["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+
+#exercise 2
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    if x == "banana":
+        continue
+    print(x)
+
+#exercise 3
+for x in range(6):
+    print(x)
+
+#exercise 4
+fruits=["apple", "banana", "cherry"]
+for x in fruits:
+    if x=="banana":
+        break
+    print(x)
