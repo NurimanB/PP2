bb4f08e051c5b013a85127ef6baa6f2dd9eb8713
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-01-25 11:37:15 +0600

    commited

--- lab2.py
+++ lab2.py
@@ -119,3 +119,17 @@ fruits.discard("banana")
 
 #PYTHON Dictionaries
 #exercise 1
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(car.get("model"))
+
+#exercise 2
+car =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+car["year"] = 2020
