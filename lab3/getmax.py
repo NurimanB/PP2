28e2205de7e7c9b43d43916f62960984aeaebcaa
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-02-05 22:31:38 +0600

    commited

---
+++ lab3/getmax.py
@@ -0,0 +1,9 @@
+def get_max():
+    a = int(input("enter a number: "))
+    b = int(input("enter a number: "))
+    c = int(input("enter a number: "))
+    return max(a, b, c)
+
+result = get_max()
+print(result)
+
