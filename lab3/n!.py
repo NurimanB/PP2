8c09213f709e5457c1328329a596ace360b991a5
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-02-07 21:12:34 +0600

    commited

---
+++ lab3/n!.py
@@ -0,0 +1,13 @@
+def calculate_factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * calculate_factorial(n - 1)
+
+# Example usage:
+number = int(input("Enter a positive integer: "))
+if number < 0:
+    print("Please enter a positive integer.")
+else:
+    result = calculate_factorial(number)
+    print(f"The factorial of {number} is: {result}")
