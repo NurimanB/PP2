c62c49af722a8017d557c54a702761bc37e65a79
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-02-07 20:46:29 +0600

    commited

---
+++ lab3/iseven.py
@@ -0,0 +1,8 @@
+def is_even():    
+    num = int(input("Enter a number:"))
+    if num%2==0:        
+        print("True")
+    else:       
+        print("False")
+        
+is_even()
---
+++ lab3/rstring.py
@@ -0,0 +1,6 @@
+def reverse_string():   
+    st = input("Enter a string: ")
+    return st[::-1]
+
+rstring = reverse_string()
+print(rstring)
