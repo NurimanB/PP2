4863d841a203ad5d96dbe67fe5c6f12427587b0a
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-03-29 12:08:31 +0500

    lab7 done

--- lab7/player/songs/music.py
+++ lab7/player/songs/music.py
@@ -1,38 +1,36 @@
 import pygame
 import os
 
-# Initialize pygame
 pygame.init()
 
-# Set up display
+# создание экрана
 screen_width = 375
 screen_height = 812
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# Load iPhone screen image
+# загрузка айфона
 iphone_img = pygame.image.load('D:\VsCode\python\lab7\player\img/phone.jpeg')
 iphone_img = pygame.transform.scale(iphone_img, (screen_width, screen_height))
 
-# Set the directory where your music files are located
 music_dir = 'D:\VsCode\python\lab7\player\songs'
 
-# Get a list of all music files in the directory
+# лист песен в директорий
 music_files = [f for f in os.listdir(music_dir) if f.endswith('.mp3')]
 
-# Initialize variables 
+# инициализировать переменные
 current_track = 0
 paused = False
 
-# Load the first track
+# играть первый трек
 pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
 
-# Play the loaded track
+# играть загруженный трек
 pygame.mixer.music.play()
 
-# Font
+# стил текста
 font = pygame.font.SysFont(None, 30) 
 
-# Main loop
+# главный код
 running = True
 while running:
     for event in pygame.event.get():
@@ -55,21 +53,17 @@ while running:
                 pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
                 pygame.mixer.music.play()
 
-    # Fill the screen with white
+    # цвет экрана
     screen.fill((255, 255, 255))
-
-    # Draw iPhone screen
     screen.blit(iphone_img, (0, 0))
 
-
-    # Display music name
+    # показывать имя песни
     music_name = music_files[current_track].split('.')[0]
     text = font.render(music_name, True, (0, 0, 0))
     text_rect = text.get_rect(center=(screen_width // 2, 510))
     screen.blit(text, text_rect)
 
-    # Update the display
+    #отрисовка названия текущего трека
     pygame.display.flip()
 
-# Quit pygame
 pygame.quit()
