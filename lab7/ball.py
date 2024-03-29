9610c90f151e756e13625b7f162f3e8425af4f13
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-03-29 12:09:41 +0500

    lab7 done

--- lab7/ball.py
+++ lab7/ball.py
@@ -3,15 +3,12 @@ import sys
 
 pygame.init()
 
-# размеры экрана
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
-# цвета
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-# размеры и начальное положение мяча
 BALL_SIZE = 50
 BALL_RADIUS = BALL_SIZE // 2
 ball_x = (SCREEN_WIDTH - BALL_SIZE) // 2
@@ -19,7 +16,6 @@ ball_y = (SCREEN_HEIGHT - BALL_SIZE) // 2
 
 BALL_SPEED = 20
 
-# создание экрана
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("ball")
 
@@ -41,20 +37,15 @@ while running:
     elif keys[pygame.K_RIGHT]:
         ball_x += BALL_SPEED
 
-    # чтобы мяч не вышел за границы экрана
     ball_x = max(0, min(SCREEN_WIDTH - BALL_SIZE, ball_x))
     ball_y = max(0, min(SCREEN_HEIGHT - BALL_SIZE, ball_y))
 
-    # заполнение экрана белым цветом
     screen.fill(WHITE)
 
-    # рисование мяча
     pygame.draw.circle(screen, RED, (ball_x + BALL_RADIUS, ball_y + BALL_RADIUS), BALL_RADIUS)
 
-    # обновление экрана
     pygame.display.flip()
 
-    # ограничение fps
     clock.tick(60)
 
 
--- lab7/player/songs/music.py
+++ lab7/player/songs/music.py
@@ -3,34 +3,26 @@ import os
 
 pygame.init()
 
-# создание экрана
 screen_width = 375
 screen_height = 812
 screen = pygame.display.set_mode((screen_width, screen_height))
 
-# загрузка айфона
 iphone_img = pygame.image.load('D:\VsCode\python\lab7\player\img/phone.jpeg')
 iphone_img = pygame.transform.scale(iphone_img, (screen_width, screen_height))
 
 music_dir = 'D:\VsCode\python\lab7\player\songs'
 
-# лист песен в директорий
 music_files = [f for f in os.listdir(music_dir) if f.endswith('.mp3')]
 
-# инициализировать переменные
 current_track = 0
 paused = False
 
-# играть первый трек
 pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
 
-# играть загруженный трек
 pygame.mixer.music.play()
 
-# стил текста
 font = pygame.font.SysFont(None, 30) 
 
-# главный код
 running = True
 while running:
     for event in pygame.event.get():
@@ -53,17 +45,14 @@ while running:
                 pygame.mixer.music.load(os.path.join(music_dir, music_files[current_track]))
                 pygame.mixer.music.play()
 
-    # цвет экрана
     screen.fill((255, 255, 255))
     screen.blit(iphone_img, (0, 0))
 
-    # показывать имя песни
     music_name = music_files[current_track].split('.')[0]
     text = font.render(music_name, True, (0, 0, 0))
     text_rect = text.get_rect(center=(screen_width // 2, 510))
     screen.blit(text, text_rect)
 
-    #отрисовка названия текущего трека
     pygame.display.flip()
 
 pygame.quit()
