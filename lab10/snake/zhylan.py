7636f19240ef58cf6e78a8f00d13eebd6dd1c0f4
Nuriman Baltabayev <nrmnbltbv@gmail.com>
2024-05-03 00:10:56 +0500

    lab10 done

--- lab10/snake/zhylan.py
+++ lab10/snake/zhylan.py
@@ -6,44 +6,45 @@ pygame.init()
 #color
 WHITE = (255,255,255)
 RED = (255,0,0)
+DARK_GREEN = (17,119,44)
 GREEN = (0,255,0)
-BLUE = (0,0,255)
 BLACK = (0,0,0)
 YELLOW =(255, 191, 0)
 
 #clock
 clock = pygame.time.Clock()
 FPS = 10
-time_for_big_food = 10*FPS+1  # my time it is just cnt by FPS  10*FPS=10s
+big_food = 10*FPS+1  # my time it is just cnt by FPS  10*FPS=10s
 
 #screen
 l, w = 1001, 601
 screen = pygame.display.set_mode((l,w))
 running = True
-dx, dy = 0, 0
+x_axis, y_axis = 0, 0
 radius = 10
 body = [[10, 10]]
 snake_len=1
 wall = []
 wall_len = 0
-condition_kill = False
+kill_condition = False
 last_key = str("")
 
 #value
 level_value = 1
 score_value = 0
 record = 0
-#text
+
+#fonts
 font = pygame.font.SysFont("comicsansms", 24)
 font_Gameover = pygame.font.SysFont("comicsansms", 72)
-text4 = font.render("GAME OVER", True, BLACK)
+text4 = font.render("YOU LOSE", True, BLACK)
 
 
-#random cordinate  
-def rondom_c():
-    value_x = random.randrange(10, l-10)
-    value_y = random.randrange(10, w-10)
-    x1, y1 = 10 * round(value_x / 10), 10 * round(value_y /10)
+#cordinate randomiser
+def random_c():
+    x_value = random.randrange(10, l-10)
+    y_value = random.randrange(10, w-10)
+    x1, y1 = 10 * round(x_value / 10), 10 * round(y_value /10)
     condition = True
     for i in range(len(body)):
         if body[i][0]==x1 and body[i][1] == y1:
@@ -57,41 +58,41 @@ def rondom_c():
     if condition == True:
         return x1, y1
     else:
-        x1, y1 = rondom_c()
+        x1, y1 = random_c()
         return x1, y1
 
-food_x, food_y = rondom_c()
-BIG_FOOD_X, BIG_FOOD_Y = rondom_c()
+food_x, food_y = random_c()
+BIG_FOOD_X, BIG_FOOD_Y = random_c()
 
 def kill_yourself():
-    global condition_kill
+    global kill_condition
     for i in range(1,len(body)):
         if body[0][0] == body[i][0] and body[0][1] == body[i][1]:
-            condition_kill = True
+            kill_condition = True
             break
-    return condition_kill
+    return kill_condition
     
 
-def kill_wall_line():
-    global condition_kill
+def tall_wall_kill():
+    global kill_condition
     for i in range(100,400):
         if body[0][0] == 300 and body[0][1] == i:
-            condition_kill = True
+            kill_condition = True
             break
-    return condition_kill
+    return kill_condition
 
-def kill_wall():  
-    global condition_kill
+def wall_kill():  
+    global kill_condition
     for i in range(len(wall)):
         if body[0][0] == wall[i][0] and body[0][1] == wall[i][1]:
-            condition_kill = True
+            kill_condition = True
             break
 
-    return condition_kill
+    return kill_condition
    
 
 
-#SCL
+#SQL
 conn = psycopg2.connect(
 	database="postgres",
 	user='postgres',
@@ -99,16 +100,16 @@ conn = psycopg2.connect(
 	host='localhost',
 	#port= '5432'
 )
-cursor = conn.cursor()
+con = conn.cursor()
 conn.autocommit = True
 
-login_name = str(input('Pls enter your login: '))
+login_name = str(input('Enter your login: '))
 sql = f"select * from snakedata where user_login =\'{login_name}\'"
-cursor.execute(sql)
-info = cursor.fetchall()
+con.execute(sql)
+info = con.fetchall()
 
 if len(info) > 0:
-    print("You already have account, looading your data")
+    print("Found an existing account, loading your data")
     score_value = info[0][1]
     level_value = info[0][2]
     FPS = info[0][3]
@@ -116,12 +117,12 @@ if len(info) > 0:
     wall_len = int(info[0][5])
     body[0][0] = int(info[0][6])
     body[0][1] = int(info[0][7])
-    dx = -10
+    x_axis = -10
     record = info[0][8]
     for i in range(1,snake_len):
         body.append([-i*200,-i*200])
     for j in range(wall_len):
-        food_x2, food_y2 = rondom_c()
+        food_x2, food_y2 = random_c()
         wall.append([food_x2, food_y2])
     print(body)
 
@@ -131,12 +132,12 @@ if len(info) > 0:
 
 
 else:
-    print("this login didnt regestrated, So now you have new acount")
+    print("this User isn't registered, So we created a new account")
     sql_insert = f"INSERT INTO snakedata(user_login, last_score, last_level, last_FPS, snake_len, wall_len, snake_x, snake_y, record) VALUES( \'{login_name}\',\'{score_value}\',\'{level_value}\',\'{FPS}\', \'{snake_len}\', \'{wall_len}\',\'{body[0][0]}\', \'{body[0][1]}\', \'{record}\' )"
-    cursor.execute(sql_insert)
+    con.execute(sql_insert)
 
 
-def GAME_OVER():
+def game_over():
     global running
     global sql_insert1
     global score_value
@@ -147,11 +148,11 @@ def GAME_OVER():
     screen.blit(text3, (850,65))
     screen.blit(text4, (425,225))
     sql_insert1 = f"UPDATE snakedata set  last_score = 0, last_level = 1, last_FPS = 10, snake_len = 1, wall_len = 0, snake_x=10, snake_y=10  where user_login = \'{login_name}\' "
-    cursor.execute(sql_insert1)
+    con.execute(sql_insert1)
     if score_value > record:
         sql_insert1 = f"UPDATE snakedata set record = \'{score_value}\'  where user_login = \'{login_name}\' "
         record = score_value
-        cursor.execute(sql_insert1)
+        con.execute(sql_insert1)
   
 
 
@@ -173,29 +174,29 @@ while running:
         #control
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT and last_key!="K_LEFT":
-                dx = 10
-                dy = 0
+                x_axis = 10
+                y_axis = 0
                 last_key = "K_RIGHT"
 
             if event.key == pygame.K_LEFT and last_key!="K_RIGHT":
-                dx = -10
-                dy = 0
+                x_axis = -10
+                y_axis = 0
                 last_key = "K_LEFT"
 
             if event.key == pygame.K_UP and last_key!="K_DOWN":
-                dx = 0
-                dy = -10
+                x_axis = 0
+                y_axis = -10
                 last_key = "K_UP"
 
             if event.key == pygame.K_DOWN and last_key!="K_UP":
-                dx = 0
-                dy = 10
+                x_axis = 0
+                y_axis = 10
                 last_key = "K_DOWN"
 
-            if event.key == pygame.K_p:
+            if event.key == pygame.K_ESCAPE:
                 print("PAUSE")
                 sql_insert = f"UPDATE snakedata set last_score =\'{score_value}\',last_level =\'{level_value}\',last_FPS =\'{FPS}\',snake_len=\'{snake_len}\',wall_len=\'{wall_len}\', snake_x=\'{body[0][0]}\', snake_y=\'{body[0][1]}\', record = \'{record}\' where user_login = \'{login_name}\'; "
-                cursor.execute(sql_insert)
+                con.execute(sql_insert)
                 running = False
                 
                 
@@ -210,17 +211,17 @@ while running:
 
     #eating
     if body[0][0]==food_x and body[0][1]==food_y:
-        food_x, food_y = rondom_c()
+        food_x, food_y = random_c()
         body.append([0, 0])
         score_value += 10
-        time_for_big_food = 10*FPS +1
+        big_food = 10*FPS +1
 
     
     #eating BIF FOOD
     elif body[0][0]==BIG_FOOD_X and body[0][1]== BIG_FOOD_Y:
         body.append([0, 0])
         score_value += 30
-        time_for_big_food = 10*FPS+1
+        big_food = 10*FPS+1
 
 
     #level
@@ -243,8 +244,8 @@ while running:
         body[0][0] = l-11
     
     #body coordination
-    body[0][0] += dx
-    body[0][1] += dy
+    body[0][0] += x_axis
+    body[0][1] += y_axis
 
     # main screen     
     screen.fill(BLACK)
@@ -259,14 +260,14 @@ while running:
     # Draw food
     pygame.draw.circle(screen, WHITE, (food_x, food_y), radius-5)
 
-    if time_for_big_food == 0:
-        BIG_FOOD_X, BIG_FOOD_Y = rondom_c()
+    if big_food == 0:
+        BIG_FOOD_X, BIG_FOOD_Y = random_c()
 
-    if time_for_big_food <= 10*FPS:
+    if big_food <= 10*FPS:
         pygame.draw.circle(screen, YELLOW, (BIG_FOOD_X, BIG_FOOD_Y), radius)
-        time_for_big_food += 1
+        big_food += 1
 
-    if time_for_big_food > 10*FPS:
+    if big_food > 10*FPS:
         BIG_FOOD_X, BIG_FOOD_Y = -100, -100
 
     # Draw snake body
@@ -275,34 +276,34 @@ while running:
             pygame.draw.circle(screen, GREEN, (x, y), radius)
 
     # Draw snake head
-    pygame.draw.circle(screen, RED, (body[0][0], body[0][1]), radius)
+    pygame.draw.circle(screen, DARK_GREEN, (body[0][0], body[0][1]), radius)
     
     #level 1
     if level_value == 1:
-        pygame.draw.line(screen, BLUE, (300,100),(300, 400), 10)
-        if kill_wall_line() == True:
-            print("BLUE WALL")
-            GAME_OVER()
+        pygame.draw.line(screen, RED, (300,100),(300, 400), 10)
+        if tall_wall_kill() == True:
+            print("You hit a tall wall!")
+            game_over()
 
 
     # add wall circle & add FPS next level
     if last_level != level_value:
         FPS += 3
-        food_x2, food_y2 = rondom_c()
+        food_x2, food_y2 = random_c()
         wall.append([food_x2, food_y2])
-        time_for_big_food = 0
+        big_food = 0
 
     if level_value > 1:
         for i in range(len(wall)):
-            pygame.draw.circle(screen, BLUE, (wall[i][0], wall[i][1]), radius-5)
-        if kill_wall()== True:
-            print("SMALL WALL")
-            GAME_OVER()
+            pygame.draw.circle(screen, RED, (wall[i][0], wall[i][1]), radius-5)
+        if wall_kill()== True:
+            print("You hit a small wall!")
+            game_over()
     
     # Kill yourself
     if kill_yourself()==True:
-        print("killed yourself")
-        GAME_OVER
+        print("You died.")
+        game_over
         
 
 
